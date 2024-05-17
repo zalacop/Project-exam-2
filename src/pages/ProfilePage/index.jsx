260f9c4e86badaef0ff1c67641db968e36217e43
zalacop <103320914+zalacop@users.noreply.github.com>
2024-05-17 02:17:03 +0200

    adds profile styles

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -47,35 +47,54 @@ function Profile() {
   const { venues, bookings, avatar, email, name, venueManager } = profile;
 
   return (
-    <div>
-      <div>
-        <img src={avatar.url} alt={avatar.alt} />
-        <button>Edit Profile</button>
+    <>
+      <div className="mx-auto mb-8 mt-20 flex flex-col justify-center gap-20 md:flex-row md:items-start">
+        <div className="mb-4 flex flex-col gap-5 md:mb-0 md:mr-8 md:justify-center md:text-center">
+          <img
+            src={avatar.url}
+            alt={avatar.alt}
+            className="mx-auto mb-4 h-60 w-60 md:mb-0"
+          />
+          <button className="mx-auto w-max border px-8 py-1 font-bold">
+            Edit Profile
+          </button>
+        </div>
+        <div className="my-auto border p-10 px-20 text-center md:text-left">
+          <div className="mx-auto sm:w-3/4">
+            <p className="mb-2">Email: {email}</p>
+            <p className="mb-2">Name: {name}</p>
+            <p className="mb-2">Venue Manager: {venueManager ? "Yes" : "No"}</p>
+            {venueManager && (
+              <button className="mx-auto w-max border px-8 py-1 font-bold">
+                Create New Venue
+              </button>
+            )}
+          </div>
+        </div>
       </div>
+
       <div>
-        <p>Email: {email}</p>
-        <p>Name: {name}</p>
-        <p>Venue Manager: {venueManager ? "Yes" : "No"}</p>
-        {venueManager && <button>Create New Venue</button>}
+        {venueManager && venues && venues.length > 0 && (
+          <MyVenues venues={venues} />
+        )}
+        {venueManager && (!venues || venues.length === 0) && (
+          <div className="mx-auto my-10 w-3/4 border px-10 py-5">
+            <h2 className="mb-4 text-xl font-bold">My Venues</h2>
+            <p>You currently don't have any venues.</p>
+          </div>
+        )}
       </div>
-      {venueManager && venues && venues.length > 0 && (
-        <MyVenues venues={venues} />
-      )}
-      {venueManager && (!venues || venues.length === 0) && (
-        <>
-          <h2>My Venues</h2>
-          <p>You currently don't have any venues.</p>
-        </>
-      )}
-      {bookings && bookings.length > 0 ? (
-        <MyBookings bookings={bookings} />
-      ) : (
-        <>
-          <h2>My Bookings</h2>
-          <p>You currently don't have any upcoming bookings.</p>
-        </>
-      )}
-    </div>
+
+      <div>
+        {bookings && bookings.length > 0 && <MyBookings bookings={bookings} />}
+        {bookings && bookings.length === 0 && (
+          <div className="mx-auto w-3/4 border px-10 py-5">
+            <h2 className="mb-4 text-xl font-bold">My Bookings</h2>
+            <p>You currently don't have any upcoming bookings.</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
